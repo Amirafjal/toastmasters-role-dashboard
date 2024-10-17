d82339cede7c07b9a0598ba5f2a051d09b2cb8ce
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2024-10-17 21:35:06 +0530

    4.Write the API Calls in the Service

--- toastlinker-frontend/src/app/role.service.ts
+++ toastlinker-frontend/src/app/role.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Role {
+  role_id: number;
+  role: string;
+  club: string;
+  date: string;
+  status: string;
+  interested: string[];
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RoleService {
+  private apiUrl = 'http://127.0.0.1:5000/roles'; // Flask API URL
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Get all roles
+  getRoles(): Observable<Role[]> {
+    return this.http.get<Role[]>(this.apiUrl);
+  }
+
+  // Add a new role
+  addRole(role: Role): Observable<Role> {
+    return this.http.post<Role>(this.apiUrl, role);
+  }
+
+  // Show interest in a role
+  showInterest(roleId: number, name: string): Observable<Role> {
+    return this.http.post<Role>(`${this.apiUrl}/${roleId}/interest`, { name });
+  }
 }
